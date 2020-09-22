37e7b9ceb553c522dce1e78be04386a42b98d14e
jyotirmaysenapati <senapati.jyotirmay@gmail.com>
2020-09-22 06:30:59 +0200

    22092020: loader added + kloserr link added.

---
+++ js/loader.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() {
+	$('body').addClass('loaded');
+	$('h1').css('color','#000000');
+	// setTimeout(function(){
+	// 	$('body').addClass('loaded');
+	// 	$('h1').css('color','#000000');
+	// }, 1000);
+
+});
