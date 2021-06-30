78e6c438aca5cee316000cf7705b94c3c08ac2f9
jyotirmaysenapati <senapati.jyotirmay@gmail.com>
2021-06-30 22:58:17 +0200

    test

--- js/japi.js
+++ js/japi.js
@@ -1,87 +1,3 @@
-document.currentScript = document.currentScript || (function() {
-    var scripts = document.getElementsByTagName('script');
-    return scripts[scripts.length - 1];
-})();
-let pageName = document.currentScript.getAttribute('page');
-
-$(document).ready(function() {
-    async function pushToDB(pos, log='Navigator Position') {
-        $.getJSON('https://ipapi.co/json/', function (geodata) {
-
-            const timeElapsed = Date.now();
-            const today = new Date(timeElapsed);
-            geodata.time = today.toUTCString();
-            geodata.time_timezone = 'UTC';
-            geodata.page= pageName;
-            geodata.pos = pos;
-            geodata.log = log;
-            geodata.comment = '';
-            console.log(geodata);
-
-            actualPush(geodata)
-        });
-    }
-
-    async function actualPush(geodata) {
-        $.post( "https://jyotirmays-api.herokuapp.com/v1/insertJson", geodata, function( data ) {
-            console.log(data);
-        });
-    }
-
-    if (navigator.geolocation) {
-        navigator.geolocation.watchPosition(
-            pushToDB,
-            (error) => {
-                if (error.code === error.PERMISSION_DENIED) {
-                    // console.log("you denied me :-(");
-                    pushToDB(null, 'Denied Me');
-                } else {
-                    pushToDB(null, 'Some Other Error!');
-                }
-            });
-    } else {
-        pushToDB(null, 'No Navigator');
-    }
-
-    // $('#git-tooltip').tooltip();
-    $('rect').mousemove((evt) => {
-        showTooltip(evt);
-    }).mouseleave((evt) => {
-        hideTooltip();
-    })
-});
-
-function showTooltip(evt) {
-    let tooltip = document.getElementById("tooltip");
-    if(evt.target.dataset.count == undefined || evt.target.dataset.date == undefined) {
-        return false;
-    }
-    tooltip.innerHTML = evt.target.dataset.count + ' push on '+ evt.target.dataset.date;
-    tooltip.style.display = "block";
-    tooltip.style.left = evt.pageX + 10 + 'px';
-    tooltip.style.top = evt.pageY + 10 + 'px';
-}
-
-function hideTooltip() {
-    var tooltip = document.getElementById("tooltip");
-    tooltip.style.display = "none";
-}
-
-$.get("https://jyotirmays-api.herokuapp.com/v1/se/data", (data)=>{
-    console.log(data);
-    new Chart(document.getElementById("line-chart"), {
-        type: 'line',
-        data: data,
-        options: {
-            title: {
-                display: true,
-                text: 'StackOverflow Reputation '
-            }
-        }
-    });
-});
-
-
 // HIGH CHART STUFF
 // Always put japi at the end of all import in html.
 
@@ -174,4 +90,87 @@ $(document).ready(function() {
         ]
     });
 
+});
+
+document.currentScript = document.currentScript || (function() {
+    var scripts = document.getElementsByTagName('script');
+    return scripts[scripts.length - 1];
+})();
+let pageName = document.currentScript.getAttribute('page');
+
+$(document).ready(function() {
+    async function pushToDB(pos, log='Navigator Position') {
+        $.getJSON('https://ipapi.co/json/', function (geodata) {
+
+            const timeElapsed = Date.now();
+            const today = new Date(timeElapsed);
+            geodata.time = today.toUTCString();
+            geodata.time_timezone = 'UTC';
+            geodata.page= pageName;
+            geodata.pos = pos;
+            geodata.log = log;
+            geodata.comment = '';
+            console.log(geodata);
+
+            actualPush(geodata)
+        });
+    }
+
+    async function actualPush(geodata) {
+        $.post( "https://jyotirmays-api.herokuapp.com/v1/insertJson", geodata, function( data ) {
+            console.log(data);
+        });
+    }
+
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(
+            pushToDB,
+            (error) => {
+                if (error.code === error.PERMISSION_DENIED) {
+                    // console.log("you denied me :-(");
+                    pushToDB(null, 'Denied Me');
+                } else {
+                    pushToDB(null, 'Some Other Error!');
+                }
+            });
+    } else {
+        pushToDB(null, 'No Navigator');
+    }
+
+    // $('#git-tooltip').tooltip();
+    $('rect').mousemove((evt) => {
+        showTooltip(evt);
+    }).mouseleave((evt) => {
+        hideTooltip();
+    })
+});
+
+function showTooltip(evt) {
+    let tooltip = document.getElementById("tooltip");
+    if(evt.target.dataset.count == undefined || evt.target.dataset.date == undefined) {
+        return false;
+    }
+    tooltip.innerHTML = evt.target.dataset.count + ' push on '+ evt.target.dataset.date;
+    tooltip.style.display = "block";
+    tooltip.style.left = evt.pageX + 10 + 'px';
+    tooltip.style.top = evt.pageY + 10 + 'px';
+}
+
+function hideTooltip() {
+    var tooltip = document.getElementById("tooltip");
+    tooltip.style.display = "none";
+}
+
+$.get("https://jyotirmays-api.herokuapp.com/v1/se/data", (data)=>{
+    console.log(data);
+    new Chart(document.getElementById("line-chart"), {
+        type: 'line',
+        data: data,
+        options: {
+            title: {
+                display: true,
+                text: 'StackOverflow Reputation '
+            }
+        }
+    });
 });
